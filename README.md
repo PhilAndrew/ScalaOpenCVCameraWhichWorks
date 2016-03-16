8879b64db143906bb1968bb89df1fcd8753519e4
Philip Andrew <phil@orsa-studio.com>
2016-03-16 22:05:22 +0800

    Work but color wrong

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is just an example which works for me.
+Works with OpenCV 3.
+
+Able to fetch camera data and display it to the screen. The color is wrong but that doesnt matter right now. Using https://github.com/lloydmeta/sbt-opencv
--- src/main/scala/com/chimpler/javacv/FaceWebcamDetectorApp.scala
+++ src/main/scala/com/chimpler/javacv/FaceWebcamDetectorApp.scala
@@ -1,10 +1,34 @@
 package com.chimpler.javacv
 
+import java.awt.image.BufferedImage
+
 import org.bytedeco.javacpp.opencv_core._
+import org.bytedeco.javacpp.opencv_videoio.VideoCapture
 import org.bytedeco.javacv.FrameGrabber.ImageMode
 import org.bytedeco.javacv.{CanvasFrame, OpenCVFrameGrabber}
 
 object FaceWebcamDetectorApp extends App {
+
+  var b: Array[Byte] = null
+  var image: BufferedImage = null
+
+  def convert(m: Mat) {
+    var `type` = BufferedImage.TYPE_BYTE_GRAY
+    if (m.channels() > 1) {
+      //Imgproc.cvtColor(m, m2, Imgproc.COLOR_BGR2RGB)
+      `type` = BufferedImage.TYPE_3BYTE_BGR
+    }
+    val blen = m.channels() * m.cols() * m.rows()
+    if (b == null || b.length != blen) b = Array.ofDim[Byte](blen)
+
+    val byteBuffer = m.getByteBuffer
+    for (n <- 0 until blen) {
+      b(n) = byteBuffer.get(n)
+    }
+    if (image==null)
+      image = new BufferedImage(m.cols(), m.rows(), `type`)
+    image.getRaster.setDataElements(0, 0, m.cols(), m.rows(), b)
+  }
 
   val WIDTH = 1280
   val HEIGHT = 720
@@ -23,21 +47,27 @@ object FaceWebcamDetectorApp extends App {
   canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE)
 
   //Declare FrameGrabber to import output from webcam
-  val grabber = new OpenCVFrameGrabber(0)
-  grabber.setImageWidth(WIDTH)
+  val camera = new VideoCapture(0)
+/*  grabber.setImageWidth(WIDTH)
   grabber.setImageHeight(HEIGHT)
   grabber.setBitsPerPixel(CV_8U)
   grabber.setImageMode(ImageMode.COLOR)
-  grabber.start()
+  grabber.start()*/
+  val frame = new Mat()
 
   var lastRecognitionTime = 0L
 
-  val mat = new Mat(WIDTH, HEIGHT, CV_8UC3)
-  val greyMat = new Mat(WIDTH, HEIGHT, CV_8U)
-  var faces: Seq[Face] = Nil
+  //val mat = new Mat(WIDTH, HEIGHT, CV_8UC3)
+  //val greyMat = new Mat(WIDTH, HEIGHT, CV_8U)
+  //var faces: Seq[Face] = Nil
   while (true) {
-    val img = grabber.grab()
-    canvas.showImage(img)
+    camera.read(frame)
+    // convert to java.awt.Image
+
+    //val bufferedImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_4BYTE_ABGR)
+    //bufferedImage.(frame.getByteBuffer.array())
+    convert(frame)
+    canvas.showImage(image)
   }
 
 }
